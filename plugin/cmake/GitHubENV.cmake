0cef2fd2aaa3fa0ebbab36aea966042fe3816567
markus.wagner <markus.wagner.privat@gmail.com>
2026-01-02 17:37:41 +0100

    Cleanup: entferne versehentlich committete Build-Artefakte, aktualisiere .gitignore, nur Quell- und Projektdateien im Repo

--- plugin/cmake/GitHubENV.cmake
+++
@@ -1,9 +0,0 @@
-# This outputs configuration for GitHub Actions
-
-# Set environment variables for CI/CD
-if(DEFINED ENV{GITHUB_ENV})
-    file(APPEND $ENV{GITHUB_ENV} "PROJECT_NAME=${PROJECT_NAME}\n")
-    file(APPEND $ENV{GITHUB_ENV} "PRODUCT_NAME=${PRODUCT_NAME}\n")
-    file(APPEND $ENV{GITHUB_ENV} "PROJECT_VERSION=${PROJECT_VERSION}\n")
-    file(APPEND $ENV{GITHUB_ENV} "COMPANY_NAME=${COMPANY_NAME}\n")
-endif()
